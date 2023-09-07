9a9e2b4bb7fb225505b95cb7a2e53d9029831ef6
rathesh77 <rathesh.charles@outlook.fr>
2023-09-07 14:26:52 +0200

    add fs

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const router = require('./routes/flights.routes.js')
 
+const fs = require('fs')
 
+if (!fs.existsSync('./books.json')) {
+  fs.appendFileSync('./books.json', '')
+}
 app.use(router);
 
 app.listen(8080, function (){
