228e6355546a97620b29749ebb174204e1e6f2df
rathesh77 <rathesh.charles@outlook.fr>
2023-09-07 13:57:50 +0200

    add routes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+const router = require('./routes/flights.routes.js')
+
+
+app.use(router);
 
 app.listen(8080, function (){
   console.log('app is listening on port 8080')
---
+++ routes/flights.routes.js
@@ -0,0 +1,11 @@
+const router = express.Router();
+
+router.get('/flights', (req, res)=> {
+
+})
+
+router.post('/book', (req, res)=> {
+
+})
+
+module.exports = router
