0af0347c840a18d21604d207c15d0522967c4391
rathesh77 <rathesh.charles@outlook.fr>
2023-09-07 15:55:03 +0200

    add cors

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const router = require('./routes/flights.routes.js')
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const fs = require('fs')
 
 if (!fs.existsSync('./books.json')) {
   fs.writeFileSync('./books.json', '')
 }
+app.use(cors())
 app.use(bodyParser.json());
-
 app.use(router);
 
 app.listen(8080, function (){
