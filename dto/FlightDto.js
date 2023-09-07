301de434d11225bedbfec22788332e8d545ad9d5
rathesh77 <rathesh.charles@outlook.fr>
2023-09-07 14:45:28 +0200

    update FlightDto.js

--- dto/FlightDto.js
+++ dto/FlightDto.js
@@ -6,7 +6,7 @@ class FlightDto {
     constructor(data) {
         this.id = data.id;
         this.airportDeparture = data.airportDeparture;
-        this.airPortArrival = data.airPortArrival;
+        this.airportArrival = data.airportArrival;
     }
 }
 
