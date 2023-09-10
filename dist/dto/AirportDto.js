e1024fad0b2d8feaff255341264cb4ec6b5e298c
rathesh77 <rathesh.charles@outlook.fr>
2023-09-10 20:04:47 +0200

    removed dist folder

--- dist/dto/AirportDto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class AirportDto {
-    constructor(data) {
-        this.name = data.name;
-        this.acronym = data.acronym;
-    }
-}
-exports.default = AirportDto;
