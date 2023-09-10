f2638670c055057ca0267e443d23630705a7930e
rathesh77 <rathesh.charles@outlook.fr>
2023-09-10 15:38:54 +0200

    cleaned getDiscountForFlights method

--- services/DiscountService.js
+++ services/DiscountService.js
@@ -1,16 +1,18 @@
+const FlightDto = require("../dto/FlightDto")
 const InMemoryData = require("../utils/inMemoryData")
 
 class DiscountService {
 
     static getDiscountForFlights(flights) {
         return flights.map(f => {
-            const discounts = InMemoryData.discounts.filter(d => d.flight.id === f.id).map((d) => {
-                const discountPrice = f.price -  (f.price  * (d.percent / 100))
+            const price = parseFloat(f.price)
+            const discounts = InMemoryData.discounts.filter(d => d.flightId === f.id).map((d) => {
+                const discountPrice = price -  (price  * (d.percent / 100))
                 
                 let discount = {...d, discountPrice }
                 return discount
             })
-            return {...f, discounts}
+            return new FlightDto({...f, discounts})
         })
     }
 }
