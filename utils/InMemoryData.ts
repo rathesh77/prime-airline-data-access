49a12fd6d4cf4cc93bda2c46c071b6ecdd55d8d4
rathesh77 <rathesh.charles@outlook.fr>
2023-09-22 11:22:05 +0200

    make use of external airports api

--- utils/InMemoryData.ts
+++ utils/InMemoryData.ts
@@ -8,6 +8,7 @@ import { bookingHistoryDto } from '../dtos/bookingHistory.dto';
 class InMemoryData {
 
   static CURRENCY_API_URL = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml';
+  static AIRPORTS_API_URL = 'https://3ddc-185-235-207-212.ngrok-free.app/airports';
 
   static currencies = (async () => {
     const dataXml = await axios.get(InMemoryData.CURRENCY_API_URL);
@@ -15,63 +16,90 @@ class InMemoryData {
     return (await parser.parseStringPromise(dataXml.data))['gesmes:Envelope'].Cube[0].Cube[0].Cube;
   })();
 
-  
+
+  static airports = (async () => {
+    const response = await axios.get(InMemoryData.AIRPORTS_API_URL);
+    const fetchedAirports = response.data;
+
+    const CDG = fetchedAirports.find((airport: {code: string, name: string}) => airport.code === 'CDG');
+    const LAX = fetchedAirports.find((airport: {code: string, name: string}) => airport.code === 'LAX');
+    const LAD = fetchedAirports.find((airport: {code: string, name: string}) => airport.code === 'LAD');
+    const JFK = fetchedAirports.find((airport: {code: string, name: string}) => airport.code === 'JFK');
+    const DTW = fetchedAirports.find((airport: {code: string, name: string}) => airport.code === 'DTW');
+
+    const updatedFlights = [
+      ({
+        'id': 1,
+        'airportDeparture': CDG,
+        'airportArrival': JFK,
+        'price': 1000,
+        'seats': 2,
+        'date': '',
+        'isLongDuration': true,
+        'meals': [
+          { 'name': 'veggie', 'quantity': 45 },
+          { 'name': 'chips', 'quantity': 10 },
+          { 'name': 'polloSandwich', 'quantity': 8 },
+          { 'name': 'pasta', 'quantity': 30 }
+        ],
+        'planeId': 1,
+      }) as FlightDto,
+      ({
+        'id': 2,
+        'airportDeparture': CDG,
+        'airportArrival': DTW,
+        'price': 700,
+        'seats': 2,
+        'date': '',
+        'isLongDuration': true,
+        'meals': [
+          { 'name': 'veggie', 'quantity': 5 },
+          { 'name': 'chips', 'quantity': 10 },
+          { 'name': 'polloSandwich', 'quantity': 80 },
+          { 'name': 'pasta', 'quantity': 30 }
+        ],
+        'planeId': 2
+      }) as FlightDto,
+      ({
+        'id': 3,
+        'airportDeparture': JFK,
+        'airportArrival': DTW,
+        'price': 300,
+        'seats': 2,
+        'date': '',
+        'isLongDuration': false,
+        'meals': [],
+        'planeId': 3
+      }) as FlightDto,
+      ({
+        'id': 4,
+        'airportDeparture': JFK,
+        'airportArrival': DTW,
+        'price': 300,
+        'seats': 2,
+        'date': '',
+        'isLongDuration': false,
+        'meals': [],
+        'planeId': 3,
+        'escale': DTW
+      }) as FlightDto,
+      ({
+        'id': 5,
+        'airportDeparture': LAX,
+        'airportArrival': LAD,
+        'price': 1200,
+        'seats': 4,
+        'date': '',
+        'isLongDuration': false,
+        'meals': [],
+        'planeId': 3,
+        'escale': JFK
+      }) as FlightDto,
+    ];
+    InMemoryData.flights = updatedFlights;
+  })();
+
   static flights: FlightDto[] = [
-    ({
-      'id': 1,
-      'airportDeparture': 'CDG',
-      'airportArrival': 'JFK',
-      'price': 1000,
-      'seats': 2,
-      'date': '',
-      'isLongDuration': true,
-      'meals': [
-        { 'name': 'veggie', 'quantity': 45 },
-        { 'name': 'chips', 'quantity': 10 },
-        { 'name': 'polloSandwich', 'quantity': 8 },
-        { 'name': 'pasta', 'quantity': 30 }
-      ],
-      'planeId': 1,
-    }) as FlightDto,
-    ({
-      'id': 2,
-      'airportDeparture': 'CDG',
-      'airportArrival': 'DTW',
-      'price': 700,
-      'seats': 2,
-      'date': '',
-      'isLongDuration': true,
-      'meals': [
-        { 'name': 'veggie', 'quantity': 5 },
-        { 'name': 'chips', 'quantity': 10 },
-        { 'name': 'polloSandwich', 'quantity': 80 },
-        { 'name': 'pasta', 'quantity': 30 }
-      ],
-      'planeId': 2
-    }) as FlightDto,
-    ({
-      'id': 3,
-      'airportDeparture': 'JFK',
-      'airportArrival': 'DTW',
-      'price': 300,
-      'seats': 2,
-      'date': '',
-      'isLongDuration': false,
-      'meals': [],
-      'planeId': 3
-    }) as FlightDto,
-    ({
-      'id': 4,
-      'airportDeparture': 'JFK',
-      'airportArrival': 'DTW',
-      'price': 300,
-      'seats': 2,
-      'date': '',
-      'isLongDuration': false,
-      'meals': [],
-      'planeId': 3,
-      'escale': 'DTW'
-    }) as FlightDto,
   ];
 
   static discounts: DiscountDto[] = [
